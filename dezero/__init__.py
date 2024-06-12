51fb3b6238f99659f920dffae872f255a836da93
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-06-13 07:45:33 +0900

    Update: module

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -10,6 +10,7 @@ if is_simple_core:
     from dezero.core_simple import setup_variable
 else:
     from dezero.core import Variable
+    from dezero.core import Parameter
     from dezero.core import Function
     from dezero.core import using_config
     from dezero.core import no_grad
@@ -19,6 +20,7 @@ else:
     from dezero.core import Config
 
     import dezero.functions
+    import dezero.layers
     import dezero.utils
 
 setup_variable()
