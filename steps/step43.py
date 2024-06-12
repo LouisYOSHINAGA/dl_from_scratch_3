44777472defe5661186e918760fb432b9db51575
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-06-12 19:49:21 +0900

    Add: step43

---
+++ steps/step43.py
@@ -0,0 +1,47 @@
+if "__file__" in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+import numpy as np
+from dezero import Variable
+import dezero.functions as F
+
+
+def predict(x, W1, b1, W2, b2):
+    y = F.linear(x, W1, b1)
+    y = F.sigmoid(y)
+    y = F.linear(y, W2, b2)
+    return y
+
+
+if __name__ == "__main__":
+    np.random.seed(0)
+    x = np.random.rand(100, 1)
+    y = np.sin(2 * np.pi * x) + np.random.rand(100, 1)
+
+    I, H, O = 1, 10, 1
+    W1 = Variable(0.01 * np.random.randn(I, H))
+    b1 = Variable(np.zeros(H))
+    W2 = Variable(0.01 * np.random.randn(H, O))
+    b2 = Variable(np.zeros(O))
+
+    lr = 0.2
+    iters = 10000
+
+    for i in range(iters):
+        y_pred = predict(x, W1, b1, W2, b2)
+        loss = F.mean_squared_error(y, y_pred)
+
+        W1.cleargrad()
+        b1.cleargrad()
+        W2.cleargrad()
+        b2.cleargrad()
+        loss.backward()
+
+        W1.data -= lr * W1.grad.data
+        b1.data -= lr * b1.grad.data
+        W2.data -= lr * W2.grad.data
+        b2.data -= lr * b2.grad.data
+
+        if i % 1000 == 0:
+            print(loss)
