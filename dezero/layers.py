158aceb1a7f3a5cab50eaa8a6a2397b063156b46
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-07-02 23:48:53 +0900

    Update: module

--- dezero/layers.py
+++ dezero/layers.py
@@ -1,10 +1,10 @@
+import weakref
+import os
 import numpy as np
 import dezero.functions as F
 from dezero.core import Parameter
 from dezero import cuda
 from dezero.utils import pair
-import weakref
-import os
 
 
 class Layer:
@@ -164,4 +164,43 @@ class RNN(Layer):
         else:
             h_new = F.tanh(self.x2h(x) + self.h2h(self.h))
         self.h = h_new
+        return h_new
+
+
+class LSTM(Layer):
+    def __init__(self, hidden_size, in_size=None):
+        super().__init__()
+        H, I = hidden_size, in_size
+        self.x2f = Linear(H, in_size=I)
+        self.x2i = Linear(H, in_size=I)
+        self.x2o = Linear(H, in_size=I)
+        self.x2u = Linear(H, in_size=I)
+        self.h2f = Linear(H, in_size=I, nobias=True)
+        self.h2i = Linear(H, in_size=I, nobias=True)
+        self.h2o = Linear(H, in_size=I, nobias=True)
+        self.h2u = Linear(H, in_size=I, nobias=True)
+        self.reset_state()
+
+    def reset_state(self):
+        self.h = None
+        self.c = None
+
+    def forward(self, x):
+        if self.h is None:
+            f = F.sigmoid(self.x2f(x))
+            i = F.sigmoid(self.x2i(x))
+            o = F.sigmoid(self.x2o(x))
+            u = F.tanh(self.x2u(x))
+        else:
+            f = F.sigmoid(self.x2f(x) + self.h2f(self.h))
+            i = F.sigmoid(self.x2i(x) + self.h2i(self.h))
+            o = F.sigmoid(self.x2o(x) + self.h2o(self.h))
+            u = F.tanh(self.x2u(x) + self.h2u(self.h))
+
+        if self.c is None:
+            c_new = i * u
+        else:
+            c_new = (f * self.c) + (i * u)
+        h_new = o * F.tanh(c_new)
+        self.h, self.c = h_new, c_new
         return h_new
--- dezero/optimizers.py
+++ dezero/optimizers.py
@@ -1,4 +1,3 @@
-import numpy as np
 import math
 from dezero import cuda
 
