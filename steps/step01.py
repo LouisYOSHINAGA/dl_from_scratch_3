d5c768d4305ba53514c72e1f6947687e9551debe
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-10-09 23:44:02 +0900

    Update: step01

--- steps/step01.py
+++ steps/step01.py
@@ -1,14 +1,12 @@
+import numpy as np
+
 class Variable:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, data: np.ndarray) -> None:
+        self.data: np.ndarray = data
 
+data: np.ndarray = np.array(1.0)
+x = Variable(data)
+print(x.data)
 
-if __name__ == "__main__":
-    import numpy as np
-
-    data = np.array(1.0)
-    x = Variable(data)
-    print(x.data)
-
-    x.data = np.array(2.0)
-    print(x.data)
+x.data = np.array(2.0)
+print(x.data)
