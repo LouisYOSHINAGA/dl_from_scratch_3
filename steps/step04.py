9bf0467fc123577e08c32f37728fb6000fb9e2cf
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-10-10 23:26:58 +0900

    Update: step04

--- steps/step04.py
+++ steps/step04.py
@@ -1,42 +1,45 @@
 import numpy as np
+from typing import Callable
+
 
 class Variable:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, data: np.ndarray) -> None:
+        self.data: np.ndarray = data
+
 
 class Function:
-    def __call__(self, input):
-        x = input.data
-        y = self.forward(x)
-        output = Variable(y)
-        return output
+    def __call__(self, input: Variable) -> Variable:
+        x: np.ndarray = input.data
+        y: np.ndarray = self.forward(x)
+        return Variable(y)
 
-    def forward(self, x):
+    def forward(self, x: np.ndarray) -> np.ndarray:
         raise NotImplementedError()
 
+
 class Square(Function):
-    def forward(self, x):
-        return x**2
+    def forward(self, x: Variable) -> Variable:
+        return x ** 2
+
 
 class Exp(Function):
-    def forward(self, x):
+    def forward(self, x: Variable) -> Variable:
         return np.exp(x)
 
-def numerical_diff(f, x, eps=1e-4):
-    x0 = Variable(x.data - eps)
-    x1 = Variable(x.data + eps)
-    y0 = f(x0)
-    y1 = f(x1)
+
+def numerical_diff(f: Callable[[Variable], Variable], x: Variable, eps: float =1e-4) -> np.ndarray:
+    y0: Variable = f(Variable(x.data - eps))
+    y1: Variable = f(Variable(x.data + eps))
     return (y1.data - y0.data) / (2 * eps)
 
 
 if __name__ == "__main__":
-    def f(x):
-        A = Square()
-        B = Exp()
-        C = Square()
+    def f(x: Variable) -> Variable:
+        A: Callable[[Variable], Variable] = Square()
+        B: Callable[[Variable], Variable] = Exp()
+        C: Callable[[Variable], Variable] = Square()
         return C(B(A(x)))
 
     x = Variable(np.array(0.5))
-    dy = numerical_diff(f, x)
+    dy: np.ndarray = numerical_diff(f, x)
     print(dy)
