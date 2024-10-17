d1c363bd3618d7e9ba2ce2fd92cd3ef2adeaf4da
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-10-17 22:08:26 +0900

    Update: step11

--- steps/step11.py
+++ steps/step11.py
@@ -1,118 +1,99 @@
 import numpy as np
-import unittest
+from typing import Callable, Any, Self
 
 
 class Variable:
-    def __init__(self, data):
+    def __init__(self, data: np.ndarray) -> None:
         if data is not None:
             if not isinstance(data, np.ndarray):
                 raise TypeError(f"{type(data)} is not supported.")
+        self.data: np.ndarray = data
+        self.grad: np.ndarray|None = None
+        self.creator: Callable[[Any], Self] = None
 
-        self.data = data
-        self.grad = None
-        self.creator = None
-
-    def set_creator(self, func):
+    def set_creator(self, func: Callable[[Any], Self]) -> None:
         self.creator = func
 
-    def backward(self):
+    def backward(self) -> None:
+        """
+            example:
+                L = s( r( q(u) + p(t) ) ) )  // L = s(w), w = r(v), v = q(u) + p(t)
+
+                dL/du = dL/dw * dw/dv * dv/du   // dL/du  = s'(w) * r'(v) * q'(u)
+                      = w.grad * dw/dv * dv/du  // w.grad = s'(w)
+                      = v.grad * dv/du          // v.grad = w.grad * r'(v)
+                      = u.grad                  // u.grad = v.grad * q'(u)
+                dL/dt = dL/dw * dw/dv * dv/dt   // dL/du  = s'(w) * r'(v) * p'(t)
+                      = w.grad * dw/dv * dv/dt  // w.grad = s'(w)
+                      = v.grad * dv/dt          // v.grad = w.grad * r'(v)
+                      = t.grad                  // t.grad = v.grad * p'(t)
+
+                funcs = [s]
+                f = s, x = w, y = L          // L = s(w)
+                w.grad = s.backward(L.grad)  // dL/dw = dL/ds * s'(w)
+
+                funcs = [r]
+                f = r, x = v, y = w          // w = r(v)
+                v.grad = r.backward(w.grad)  // dL/dv = dL/dw * r'(v)
+
+                funcs = [p, q]
+                f = q, y = v, x = u          // v = q(u)
+                u.grad = q.backward(v.grad)  // dL/du = dL/dv * q'(u)
+
+                funcs = [p]
+                f = p, y = v, x = t          // v = p(t)
+                u.grad = p.backward(v.grad)  // dL/du = dL/dv * p'(t)
+        """
         if self.grad is None:
             self.grad = np.ones_like(self.data)
 
-        funcs = [self.creator]
+        funcs: list[Callable[[Any], Variable]] = [self.creator]
         while funcs:
-            f = funcs.pop()
-            x, y = f.input, f.output
+            f: Callable[[Any], Variable] = funcs.pop()
+            x: Variable = f.input
+            y: Variable = f.output
+
             x.grad = f.backward(y.grad)
             if x.creator is not None:
                 funcs.append(x.creator)
 
 
+def as_array(x: int|float|np.ndarray) -> np.ndarray:
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+
 class Function:
-    def __call__(self, inputs):
-        as_array = lambda x: np.array(x) if np.isscalar(x) else x
-        xs = [x.data  for x in inputs]
-        ys = self.forward(xs)
-        outputs = [Variable(as_array(y)) for y in ys]
+    def __call__(self, inputs: list[Variable]) -> list[Variable]:
+        xs: list[np.ndarray] = [x.data for x in inputs]
+        ys: list[np.ndarray] = self.forward(xs)
+
+        outputs: list[Variable] = [Variable(as_array(y)) for y in ys]
         for output in outputs:
             output.set_creator(self)
-        self.inputs = inputs
-        self.outputs = outputs
+
+        self.inputs: list[Variable] = inputs
+        self.outputs: list[Variable] = outputs
         return outputs
 
-    def forward(self, x):
+    def forward(self, xs: list[np.ndarray]) -> list[np.ndarray]:
         raise NotImplementedError()
 
-    def backward(self, gy):
+    def backward(self, gys: list[np.ndarray]) -> list[np.ndarray]:
         raise NotImplementedError()
-
-
-class Square(Function):
-    def forward(self, x):
-        return x**2
-
-    def backward(self, gy):
-        x = self.input.data
-        gx = 2 * x * gy
-        return gx
-
-def square(x):
-    return Square()(x)
-
-class SquareTest(unittest.TestCase):
-    def test_forward(self):
-        x = Variable(np.array(2.0))
-        y = square(x)
-        expected = np.array(4.0)
-        self.assertEqual(y.data, expected)
-
-    def test_backward(self):
-        x = Variable(np.array(3.0))
-        y = square(x)
-        y.backward()
-        expected = np.array(6.0)
-        self.assertEqual(x.grad, expected)
-
-    def test_gradient_check(self):
-        x = Variable(np.random.rand(1))
-        y = square(x)
-        y.backward()
-        num_grad = numerical_diff(square, x)
-        flg = np.allclose(x.grad, num_grad)
-        self.assertTrue(flg)
-
-
-class Exp(Function):
-    def forward(self, x):
-        return np.exp(x)
-
-    def backward(self, gy):
-        x = self.input.data
-        gx = np.exp(x) * gy
-        return gx
-
-def exp(x):
-    return Exp()(x)
 
 
 class Add(Function):
-    def forward(self, xs):
+    def forward(self, xs: list[np.ndarray]) -> list[np.ndarray]:
         x0, x1 = xs
-        y = x0 + x1
-        return (y, )
-
-
-def numerical_diff(f, x, eps=1e-4):
-    x0 = Variable(x.data - eps)
-    x1 = Variable(x.data + eps)
-    y0 = f(x0)
-    y1 = f(x1)
-    return (y1.data - y0.data) / (2 * eps)
+        y: np.ndarray = x0 + x1
+        return [y]
 
 
 if __name__ == "__main__":
-    xs = [Variable(np.array(2)), Variable(np.array(3))]
-    f = Add()
-    ys = f(xs)
-    y = ys[0]
+    f: Callable[[list[np.ndarray]], list[np.ndarray]] = Add()
+    xs: list[Variable] = [Variable(np.array(2)), Variable(np.array(3))]
+    ys: list[Variable] = f(xs)
+    y: Variable = ys[0]
     print(y.data)
