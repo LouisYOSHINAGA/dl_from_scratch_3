d5d278dbf0c9d1b34182e1928eb69e9ac3d736c5
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-11-08 23:11:46 +0900

    Update: step28

--- steps/step28.py
+++ steps/step28.py
@@ -1,32 +1,23 @@
 if "__file__" in globals():
     import os, sys
     sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-
 import numpy as np
 from dezero import Variable
 
 
-def rosenbrock(x0, x1):
-    y = 100 * (x1 - x0 ** 2) ** 2 + (x0 - 1) ** 2
-    return y
+def rosenbrock(x0: Variable, x1: Variable) -> Variable:
+    return 100 * (x1 - x0 ** 2) ** 2 + (x0 - 1) ** 2
 
 
 if __name__ == "__main__":
-    x0 = Variable(np.array(0.0))
-    x1 = Variable(np.array(2.0))
-    y = rosenbrock(x0, x1)
-    y.backward()
-    print(x0.grad, x1.grad)
-
+    lr: float = 0.001
+    iters: int = 50000
 
     x0 = Variable(np.array(0.0))
     x1 = Variable(np.array(2.0))
-    lr = 0.001
-    iters = 1000
 
     for i in range(iters):
-        print(x0, x1)
-        y = rosenbrock(x0, x1)
+        y: Variable = rosenbrock(x0, x1)
 
         x0.cleargrad()
         x1.cleargrad()
@@ -34,3 +25,6 @@ if __name__ == "__main__":
 
         x0.data -= lr * x0.grad
         x1.data -= lr * x1.grad
+
+        if i % 1000 == 0:
+            print(f"{x0=}, {x1=}")
