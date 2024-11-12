33e45af21a629a45763073527f3f431f302cc576
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-11-12 23:04:02 +0900

    Update: step36

--- steps/step36.py
+++ steps/step36.py
@@ -1,18 +1,23 @@
 if "__file__" in globals():
     import os, sys
     sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-
 import numpy as np
 from dezero import Variable
 
 
 if __name__ == "__main__":
+    """
+        dy/dx = 2x
+        z = (dy/dx)^3 + y = 8x^3 + x^2
+        dz/dx = 24x^2 + 2x
+    """
     x = Variable(np.array(2.0))
-    y = x ** 2
+    y: Variable = x ** 2
     y.backward(create_graph=True)
-    gx = x.grad
+
+    gx: Variable = x.grad
     x.cleargrad()
 
-    z = gx ** 3 + y
+    z: Variable = gx ** 3 + y
     z.backward()
-    print(x.grad)
+    print(f"{x.grad=}")
