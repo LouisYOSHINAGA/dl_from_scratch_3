40e8bd45fbdab54ce6de7fbece82245b3ab4a2e0
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-11-12 22:48:07 +0900

    Update: step34

--- steps/step34.py
+++ steps/step34.py
@@ -1,7 +1,6 @@
 if "__file__" in globals():
     import os, sys
     sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-
 import numpy as np
 from dezero import Variable
 import dezero.functions as F
@@ -10,17 +9,18 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     x = Variable(np.linspace(-7, 7, 200))
-    y = F.sin(x)
+    y: Variable = F.sin(x)
     y.backward(create_graph=True)
 
-    logs = [y.data.flatten()]
+    logs: list[np.ndarray] = [y.data.flatten()]
 
     for i in range(3):
         logs.append(x.grad.data.flatten())
-        gx = x.grad
+        gx: Variable = x.grad
         x.cleargrad()
         gx.backward(create_graph=True)
 
+    # fig. 34-1
     labels = ["y=sin(x)", "y'", "y''", "y'''"]
     for i, v in enumerate(logs):
         plt.plot(x.data, logs[i], label=labels[i])
