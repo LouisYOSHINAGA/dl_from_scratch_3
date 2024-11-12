aeca86357117b1a2311eca7175041998f31d498a
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-11-12 22:35:35 +0900

    Update: step33

--- steps/step33.py
+++ steps/step33.py
@@ -1,37 +1,27 @@
 if "__file__" in globals():
     import os, sys
     sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-
 import numpy as np
 from dezero import Variable
 
 
-def f(x):
-    y = x ** 4 - 2 * x ** 2
-    return y
+def f(x: Variable) -> Variable:
+    return x ** 4 - 2 * x ** 2
 
 
 if __name__ == "__main__":
+    iters: int = 10
     x = Variable(np.array(2.0))
-    y = f(x)
-    y.backward(create_graph=True)
-    print(x.grad)
-
-    gx = x.grad
-    x.cleargrad()
-    gx.backward()
-    print(x.grad)
-
-
-    x = Variable(np.array(2.0))
-    iters = 10
     for i in range(iters):
-        print(i, x)
-        y = f(x)
+        print(f"iter {i:02d}: {x=}")
+
+        y: Variable = f(x)
         x.cleargrad()
         y.backward(create_graph=True)
-        gx = x.grad
+
+        gx: Variable = x.grad
         x.cleargrad()
         gx.backward()
-        gx2 = x.grad
+
+        gx2: Variable= x.grad
         x.data -= gx.data / gx2.data
