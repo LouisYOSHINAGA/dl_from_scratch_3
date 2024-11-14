cb0a7abb984e3842e4f0f49e6ac311f74b50f466
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-11-15 00:39:57 +0900

    Update: step37

--- steps/step37.py
+++ steps/step37.py
@@ -1,26 +1,21 @@
 if "__file__" in globals():
     import os, sys
     sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-
 import numpy as np
-import dezero.functions as F
 from dezero import Variable
+import dezero.functions as F
 
 
 if __name__ == "__main__":
     x = Variable(np.array([[1, 2, 3], [4, 5, 6]]))
     c = Variable(np.array([[10, 20, 30], [40, 50, 60]]))
 
-    y = F.sin(x)
-    print(y)
+    t: Variable = x + c
+    print(f"{t=}")
 
-    y = x + c
-    print(y)
-
-    t = x + c
     y = F.sum(t)
     y.backward(retain_grad=True)
-    print(y.grad)
-    print(t.grad)
-    print(x.grad)
-    print(c.grad)
+    print(f"{y.grad}")
+    print(f"{t.grad}")
+    print(f"{x.grad}")
+    print(f"{c.grad}")
