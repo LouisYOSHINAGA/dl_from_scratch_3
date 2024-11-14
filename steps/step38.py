13889697acbc22529965ba32657e5ae5b3a56e81
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-11-15 01:06:03 +0900

    Update: step38

--- steps/step38.py
+++ steps/step38.py
@@ -1,35 +1,38 @@
 if "__file__" in globals():
     import os, sys
     sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-
 import numpy as np
 from dezero import Variable
 import dezero.functions as F
 
 
 if __name__ == "__main__":
-    x = np.array([[1, 2, 3], [4, 5, 6]])
-    y = np.reshape(x, (6, ))
-    print(y)
-
     x = Variable(np.array([[1, 2, 3], [4, 5, 6]]))
-    y = F.reshape(x, (6, ))
+    y: Variable = F.reshape(x, (6, ))
     y.backward(retain_grad=True)
-    print(x.grad)
+    print(f"{x.grad=}")
+    print()
 
-    x = Variable(np.random.randn(1, 2, 3))
-    y = x.reshape([2, 3])
-    y = x.reshape(2, 3)
 
-    x = np.array([[1, 2, 3], [4, 5, 6]])
-    y = np.transpose(x)
-    print(y)
+    x = Variable(np.random.default_rng().random((1, 2, 3)))
+    print(f"{x=}")
+    y: Variable = x.reshape([2, 3])
+    print(f"{y=}")
+    z: Variable = x.reshape(2, 3)
+    print(f"{z=}")
+    print()
+
 
     x = Variable(np.array([[1, 2, 3], [4, 5, 6]]))
-    y = F.transpose(x)
+    y: Variable = F.transpose(x)
     y.backward()
-    print(x.grad)
+    print(f"{x.grad=}")
+    print()
 
-    x = Variable(np.random.rand(2, 3))
-    y = x.transpose()
-    y = x.T
+
+    x = Variable(np.random.default_rng().random((2, 3)))
+    print(f"{x=}")
+    y: Variable = x.transpose()
+    print(f"{y=}")
+    z: Variable = x.T
+    print(f"{z=}")
