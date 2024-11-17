ba7e1d6aee9bac21a77feca9b4de39a653d17293
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-11-17 23:54:07 +0900

    Update: step40

--- steps/step40.py
+++ steps/step40.py
@@ -1,37 +1,15 @@
 if "__file__" in globals():
     import os, sys
     sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-
 import numpy as np
 from dezero import Variable
-from dezero.utils import sum_to
 
 
 if __name__ == "__main__":
-    x = np.array([1, 2, 3])
-    y = np.broadcast_to(x, (2, 3))
-    print(y)
-
-    x = np.array([[1, 2, 3], [4, 5, 6]])
-    y = sum_to(x, (1, 3))
-    print(y)
-
-    y = sum_to(x, (2, 1))
-    print(y)
-
-    x0 = np.array([1, 2, 3])
-    x1 = np.array([10])
-    y = x0 + x1
-    print(y)
-
     x0 = Variable(np.array([1, 2, 3]))
     x1 = Variable(np.array([10]))
-    y = x0 + x1
-    print(y)
-
-    x0 = Variable(np.array([1, 2, 3]))
-    x1 = Variable(np.array([10]))
-    y = x0 + x1
-    print(y)
+    y: Variable = x0 + x1
     y.backward()
-    print(x1.grad)
+    print(f"{y=}")
+    print(f"{x0.grad=}")
+    print(f"{x1.grad=}")
