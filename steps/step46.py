e9e46a9e3f8a8e79e0e380ab9aec37615ebf739c
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-11-27 23:30:11 +0900

    Update: step46

--- steps/step46.py
+++ steps/step46.py
@@ -1,36 +1,32 @@
 if "__file__" in globals():
     import os, sys
     sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-
 import numpy as np
 from dezero import Variable
-from dezero import optimizers
-import dezero.functions as F
 from dezero.models import MLP
+import dezero.optimizers as O
+import dezero.functions as F
 
 
 if __name__ == "__main__":
-    np.random.seed(0)
-    x = np.random.rand(100, 1)
-    y = np.sin(2 * np.pi * x) + np.random.rand(100, 1)
+    rng: np.random.Generator = np.random.default_rng(0)
+    x: np.ndarray = rng.random(size=(100, 1))
+    y: np.ndarray = np.sin(3 * np.pi * x) + rng.random(size=(100, 1))
 
-
-    lr = 0.2
-    max_iter = 10000
-    hidden_size = 10
-
+    hidden_size: int = 10
+    lr: float = 0.2
     model = MLP((hidden_size, 1))
-    #optimizer = optimizers.SGD(lr)
-    optimizer = optimizers.MomentumSGD(lr)
-    optimizer.setup(model)
+    # optimizer = O.SGD(lr).setup(model)
+    optimizer = O.MomentumSGD(lr).setup(model)
 
+    max_iter: int = 10000
     for i in range(max_iter):
-        y_pred = model(x)
-        loss = F.mean_squared_error(y, y_pred)
+        y_pred: Variable = model(x)
+        loss: Variable = F.mean_squared_error(y, y_pred)
 
         model.cleargrads()
         loss.backward()
         optimizer.update()
 
         if i % 1000 == 0:
-            print(loss)
+            print(f"{loss=}")
