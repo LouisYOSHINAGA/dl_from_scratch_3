ad58a57ea1c46bfd6f349e4a5558d21b861c9b54
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-12-06 00:49:05 +0900

    Update: step51

--- steps/step51.py
+++ steps/step51.py
@@ -1,78 +1,81 @@
 if "__file__" in globals():
     import os, sys
     sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-
 import dezero
-import numpy as np
-import matplotlib.pyplot as plt
+from dezero.core import Variable
+from dezero.datasets import Dataset, MNIST
 from dezero.dataloaders import DataLoader
 from dezero.models import MLP
-from dezero import optimizers
 import dezero.functions as F
+import dezero.optimizers as O
+import matplotlib.pyplot as plt
 
 
-def f(x):
-    x = x.flatten()
-    x = x.astype(np.float32)
-    x /=255.0
-    return x
+def plot(train: list[float], test: list[float], label: str) -> None:
+    plt.figure(figsize=(7, 5))
+    plt.xlabel("epoch")
+    plt.ylabel(label)
+    plt.plot(range(len(train)), train, label="train")
+    plt.plot(range(len(test)), test, label="test")
+    plt.legend()
+    plt.show()
 
 
 if __name__ == "__main__":
-    train_set = dezero.datasets.MNIST(train=True, transform=None)
-    test_set = dezero.datasets.MNIST(train=False, transform=None)
-    print(len(train_set))
-    print(len(test_set))
-
-    x, t = train_set[0]
-    print(type(x), x.shape)
-    print(t)
-
-    plt.imshow(x.reshape(28, 28), cmap='gray')
-    plt.axis("off")
-    plt.show()
-    print(f"label: {t}")
-
-
-    train_set = dezero.datasets.MNIST(train=True, transform=f)
-    test_set = dezero.datasets.MNIST(train=False, transform=f)
-
-
-    max_epoch = 5
-    batch_size = 100
-    hidden_size = 1000
-
-    train_set = dezero.datasets.MNIST(train=True)
-    test_set = dezero.datasets.MNIST(train=False)
+    batch_size: int = 100
+    train_set: Dataset = MNIST(train=True)
+    test_set: Dataset = MNIST(train=False)
     train_loader = DataLoader(train_set, batch_size)
     test_loader = DataLoader(test_set, batch_size, shuffle=False)
 
+    hidden_size: int = 1000
     #model = MLP((hidden_size, 10))
     model = MLP((hidden_size, 10), activation=F.relu)
     #optimizer = optimizers.SGD().setup(model)
-    optimizer = optimizers.Adam().setup(model)
+    opt: O.Optimizer = O.Adam().setup(model)
 
+    max_epoch: int = 5
+    train_losses: list[float] = []
+    train_accs: list[float] = []
+    test_losses: list[float] = []
+    test_accs: list[float] = []
     for epoch in range(max_epoch):
-        sum_loss, sum_acc = 0, 0
+
+        # train
+        sum_loss: float = 0
+        sum_acc: float = 0
         for x, t in train_loader:
-            y = model(x)
-            loss = F.softmax_cross_entropy(y, t)
-            acc = F.accuracy(y, t)
+            y: Variable = model(x)
+            loss: Variable = F.softmax_cross_entropy(y, t)
+            acc: Variable = F.accuracy(y, t)
+
             model.cleargrads()
             loss.backward()
-            optimizer.update()
+            opt.update()
 
             sum_loss += float(loss.data) * len(t)
             sum_acc += float(acc.data) * len(t)
-        print(f"epoch {epoch+1}")
+        train_losses.append(sum_loss/len(train_set))
+        train_accs.append(sum_acc/len(train_set))
+
+        print(f"epoch {epoch+1:03d}")
         print(f"train loss: {sum_loss/len(train_set):.4f}, accuracy = {sum_acc/len(train_set):.4f}")
 
-        sum_loss, sum_acc = 0, 0
+        # test
+        sum_loss = 0
+        sum_acc = 0
         with dezero.no_grad():
             for x, t in test_loader:
-                y = model(x)
-                loss = F.softmax_cross_entropy(y, t)
-                acc = F.accuracy(y, t)
+                y: Variable = model(x)
+                loss: Variable = F.softmax_cross_entropy(y, t)
+                acc: Variable = F.accuracy(y, t)
+
                 sum_loss += float(loss.data) * len(t)
                 sum_acc += float(acc.data) * len(t)
+            test_losses.append(sum_loss/len(test_set))
+            test_accs.append(sum_acc/len(test_set))
+
         print(f"test loss: {sum_loss/len(test_set):.4f}, accuracy: {sum_acc/len(test_set):.4f}")
+
+    plot(train_losses, test_losses, "loss")
+    plot(train_accs, test_accs, "accuracy")
