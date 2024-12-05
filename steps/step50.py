3acbf0f60a0dc20d0ff3810b1c445ea4e8909024
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-12-05 23:11:58 +0900

    Update: step50

--- steps/step50.py
+++ steps/step50.py
@@ -1,95 +1,80 @@
 if "__file__" in globals():
     import os, sys
     sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-
-import numpy as np
 import dezero
+from dezero.core import Variable
 import dezero.functions as F
-from dezero.datasets import Spiral
-from dezero import DataLoader
+from dezero.datasets import Dataset, Spiral
+from dezero.dataloaders import DataLoader
 from dezero.models import MLP
-from dezero import optimizers
+import dezero.optimizers as O
+import matplotlib.pyplot as plt
 
 
-class MyIterator:
-    def __init__(self, max_cnt):
-        self.max_cnt = max_cnt
-        self.cnt = 0
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.cnt == self.max_cnt:
-            raise StopIteration()
-
-        self.cnt += 1
-        return self.cnt
+def plot(train: list[float], test: list[float], label: str) -> None:
+    plt.figure(figsize=(7, 5))
+    plt.xlabel("epoch")
+    plt.ylabel(label)
+    plt.plot(range(len(train)), train, label="train")
+    plt.plot(range(len(test)), test, label="test")
+    plt.legend()
+    plt.show()
 
 
 if __name__ == "__main__":
-    obj = MyIterator(5)
-    for x in obj:
-        print(x)
-
-
-    batch_size = 10
-    max_epoch = 1
-
-    train_set = Spiral(train=True)
-    test_set = Spiral(train=False)
-    train_loader = DataLoader(train_set, batch_size)
-    test_loader = DataLoader(test_set, batch_size, shuffle=True)
-
-    for epoch in range(max_epoch):
-        for x, t in train_loader:
-            print(x.shape, t.shape)
-            break
-
-        for x, t in test_loader:
-            print(x.shape, t.shape)
-            break
-
-
-    y = np.array([[0.2, 0.8, 0.0], [0.1, 0.9, 0.0], [0.8, 0.1, 0.1]])
-    t = np.array([1, 2, 0])
-    acc = F.accuracy(y, t)
-    print(acc)
-
-
-    max_epoch = 300
-    batch_size = 30
-    hidden_size = 10
-    lr = 1.0
-
-    train_set = dezero.datasets.Spiral(train=True)
-    test_set = dezero.datasets.Spiral(train=False)
+    batch_size: int = 30
+    train_set: Dataset = Spiral(train=True)
+    test_set: Dataset = Spiral(train=False)
     train_loader = DataLoader(train_set, batch_size)
     test_loader = DataLoader(test_set, batch_size, shuffle=False)
 
+    hidden_size: int = 10
+    lr: float = 1.0
     model = MLP((hidden_size, 3))
-    optimizer = optimizers.SGD(lr).setup(model)
+    opt: O.Optimizer = O.SGD(lr).setup(model)
 
+    max_epoch: int = 300
+    train_losses: list[float] = []
+    train_accs: list[float] = []
+    test_losses: list[float] = []
+    test_accs: list[float] = []
     for epoch in range(max_epoch):
-        sum_loss, sum_acc = 0, 0
+
+        # train
+        sum_loss: float = 0
+        sum_acc: float = 0
         for x, t in train_loader:
-            y = model(x)
-            loss = F.softmax_cross_entropy(y, t)
-            acc = F.accuracy(y, t)
+            y: Variable = model(x)
+            loss: Variable = F.softmax_cross_entropy(y, t)
+            acc: Variable = F.accuracy(y, t)
+
             model.cleargrads()
             loss.backward()
-            optimizer.update()
+            opt.update()
+
             sum_loss += float(loss.data) * len(t)
             sum_acc += float(acc.data) * len(t)
-        print(f"epoch {epoch+1}")
+        train_losses.append(sum_loss/len(train_set))
+        train_accs.append(sum_acc/len(train_set))
+
+        print(f"epoch {epoch+1:03d}")
         print(f"train loss: {sum_loss/len(train_set):.4f}, accuracy: {sum_acc/len(train_set):.4f}")
 
-        sum_loss, sum_acc = 0, 0
+        # test
+        sum_loss = 0
+        sum_acc = 0
         with dezero.no_grad():
             for x, t in test_loader:
-                y = model(x)
-                loss = F.softmax_cross_entropy(y, t)
-                acc = F.accuracy(y, t)
+                y: Variable = model(x)
+                loss: Variable = F.softmax_cross_entropy(y, t)
+                acc: Variable = F.accuracy(y, t)
+
                 sum_loss += float(loss.data) * len(t)
                 sum_acc += float(acc.data) * len(t)
+            test_losses.append(sum_loss/len(test_set))
+            test_accs.append(sum_acc/len(test_set))
+
         print(f"test loss: {sum_loss/len(test_set):.4f}, accuracy: {sum_acc/len(test_set):.4f}")
+
+    plot(train_losses, test_losses, "loss")  # fig. 50-1
+    plot(train_accs, test_accs, "accuracy")  # fig. 50-1
